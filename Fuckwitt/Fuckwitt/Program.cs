74227f8cb196be7cc5abc9ba83d4f254aea11854
Adam <Adam-Nicholls@live.nmit.ac.nz>
2017-08-04 09:30:02 +1200

    Added Fish

--- Fuckwitt/Fuckwitt/Program.cs
+++ Fuckwitt/Fuckwitt/Program.cs
@@ -9,6 +9,17 @@ namespace Fuckwitt {
         static void Main(string[] args) {
             Console.WriteLine("eat shit n die nigga");
             Console.WriteLine("eat shit n die, b0ss");
+            Fish();
+        }
+        static void Fish()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(DateTime.Now);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
